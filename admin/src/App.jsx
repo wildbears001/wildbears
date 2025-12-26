49d7c82ef319ab522e43b0d002eec282432d9585
HEVEN-STORES <heven.storess@gmail.com>
2025-12-26 15:18:57 +0530

    admin backned url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -210,7 +210,7 @@ import AdminCoupons from './pages/AdminCoupon'
 
 
 
-export const backendUrl = "http://localhost:4000"
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 export const currency = "₹ "
 
 
